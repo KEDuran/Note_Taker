0895424f08aeab595ae0ab37df94a0e5dfb2c1bd
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-04 14:33:39 -0500

    added pseudocode for the API routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,2 +1,7 @@
 // Adding the fs module to help access db.json file
 var fs = require("fs");
+
+// Adding API routes here
+// API GET request for the notes in db.json file
+// API POST request for the notes in db.json file
+// API DELETE request for the notes in db.json file
