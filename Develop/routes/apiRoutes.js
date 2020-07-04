a34d4f3985d72c2f3ddfe19e6020d3d6ca7d01ae
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-04 15:34:25 -0500

    added more code to the POST method

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -9,17 +9,31 @@ module.exports = function (app) {
 	app.get("/api/notes", function (req, res) {
 		// Used to red the notes from db.json
 		fs.readFile(path.resolve(dbDir, "db.json"), "utf8", function (err, data) {
-			// This will populate the notes data from db.json file
+			// Populates the notes data from db.json file
 			res.json(data);
 		});
 	});
 	// API POST request for the notes in db.json file
 	app.post("/api/notes", function (req, res) {
+		// declaring an empy notesObject array
 		var notesObject = [];
 		// Used to read the notes from db.json
 		fs.readFile(path.resolve(dbDir, "db.json"), "utf8", function (err, data) {
-			
+			if (err) throw err;
+			notesObject = JSON.parse(data);
 		});
+		// Appending new note to the notesObject variable
+		notesObject.push(req.body);
+		// Used to add the new notes to db.json file
+		fs.writeFile(
+			path.resolve(dbDir, "db.json"),
+			JSON.stringify(notesObject),
+			function (err) {
+				if (err) {
+					return console.log(err);
+				}
+			}
+		);
 	});
 	// API DELETE request for the notes in db.json file
 };
