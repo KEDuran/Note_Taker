818e8b31c9e060642e543f0410a1343c20e1f96a
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-03 19:06:18 -0500

    added pseudocode and module.exports

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,2 +1,11 @@
 // Adding dependency variable to access the node path module
 var path = require("path");
+
+// Adding routes for the index.html and notes.html
+// Leveraging sample code from mini-project (activity 15)
+
+// exporting these html routes for use in other files
+module.exports = function (app) {
+	// HTML GET request for index.html
+	// HTML GET request for notes.html
+};
